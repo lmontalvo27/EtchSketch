f9d88831b4bdd3e503e7626c33b04c36fde4d6ab
lmontalvo27 <39773991+lmontalvo27@users.noreply.github.com>
2026-02-23 20:52:13 -0800

    Testing grid

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//testing adding div then creating grid
+const gridContainer = document.querySelector('.grid-container');
+
+let gridSize = 16;
+let Default = gridSize * gridSize;
+
+
+
+function createGrid(grid){
+
+    for (i = 0; i < grid; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('box')
+
+
+        gridContainer.appendChild(gridItem);
+    
+    }
+   
+}
+
+createGrid(Default);
+
+//grid.appendChild();
+
+//Create a 16x16 grid of square divs using jsn inside the div grid container
+// use flexbox to have divs appear as a grid
+//borders and margins can adjust size of squares
+//set up hover effect when mouse hovers leaving a pixelated trail
