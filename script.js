bb118fd9695335ec143c7a892e465b09497acbd5
lmontalvo27 <39773991+lmontalvo27@users.noreply.github.com>
2026-02-25 11:20:04 -0800

    functional version

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 
 //testing adding div then creating grid
 const gridContainer = document.querySelector('.grid-container');
-
-let gridSize = 64;
-let Default = gridSize * gridSize;
-
+const clearButton = document.querySelector('#reset-button');
+const resizeButton = document.querySelector('#resize-button');
+//let gridSize = handleClick();
+let Default = 16;
 
 
 function createGrid(grid){
+    userInput = grid;
+    fittedGrid = grid * grid;
 
-    for (i = 0; i < grid; i++) {
+    for (i = 0; i < fittedGrid; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('box');
-        gridItem.style.height = `calc(100% /${gridSize} )`;
-        gridItem.style.width = `calc(100% / ${gridSize})`;
+        gridItem.style.height = `calc(100% /${userInput} )`;
+        gridItem.style.width = `calc(100% / ${userInput})`;
 
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'black';
+        })
 
         gridContainer.appendChild(gridItem);
     
     }
-   
 }
+function resetGrid () {
+    
+    const boxes = gridContainer.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = 'white';
+    });
 
+}
 createGrid(Default);
 
-//grid.appendChild();
+clearButton.addEventListener('click', resetGrid);
 
-//Create a 16x16 grid of square divs using jsn inside the div grid container
-// use flexbox to have divs appear as a grid
-//borders and margins can adjust size of squares
-//set up hover effect when mouse hovers leaving a pixelated trail
+function handleClick() {
+    let message = 'Enter Grid Size: ';
+    let limitMsg = 'Enter a number below 100: ';
+    let userValue = prompt(message);
+    if (userValue > 100) {
+        alert(limitMsg);
+    }
+    else {
+        const oldBoxes = gridContainer.querySelectorAll('.box');
+        oldBoxes.forEach(box=> box.remove());
+        createGrid(userValue); //updates but doesnt look correctly
+    }
+}
+
+//add button top of screen with a pop up for grid size
+//add limit of 100 and no less than 16
